16b9fcd09ce626a7234ab54b2fde3f41dca0e712
KarthikTadala <60476477+SrihariTadala@users.noreply.github.com>
2025-02-19 15:26:35 -0800

    Update WorkCard.js

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -3,13 +3,9 @@ import React from 'react';
 function WorkCard(props) {
     return (
         <div className="work-card">
+            <h4 className="card-company">{props.work.company}</h4>
             <h5 className="card-title">{props.work.title}</h5>
             <p className="card-period">{props.work.period}</p>
-            <ul className="work-responsibilities">
-                {props.work.responsibilities.map((responsibility, index) => (
-                    <li key={index}>{responsibility}</li>
-                ))}
-            </ul>
         </div>
     );
 }
